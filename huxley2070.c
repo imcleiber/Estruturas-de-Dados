ed382212ee44f46d107b818661674c8f05fafef3
Cleiber de Meireles <cleiberjunnior04@gmail.com>
2023-06-13 16:11:31 -0300

    Create huxley2070.c

---
+++ huxley2070.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int ehpar(int n){
+    if(n % 2 == 0)
+    return 1;
+    else
+    return 0;
+}
+
+void main()
+{
+    int n;
+    while(scanf("%i", &n) != 0){
+
+
+    }
+    return;
+}
