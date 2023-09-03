514fdc482aaffc1b97b61dcd1e49627138c5c7d0
Cleiber de Meireles <cleiberjunnior04@gmail.com>
2023-09-03 13:07:32 -0300

    Create huxley548.c

---
+++ huxley548.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct node{
+    int item;
+    struct lista *next;
+}NODE;
+typedef struct elemento{
+    NODE *lista;
+    NODE *next;
+}ELEMENT;
+
+typedef struct pilha{
+    ELEMENT *top;
+}STACK;
+
+void print_list(NODE *lista){
+    while(lista->next != NULL){
+        printf("%d ", lista->item);
+        lista = lista->next;
+    }
+}
+STACK *create_stack(){
+    STACK *new_stack = malloc(sizeof(STACK));
+    new_stack->top = NULL;
+    return new_stack;
+}
+
+void push(STACK *pilha, ELEMENT *item){
+    NODE *new_top = malloc(sizeof(NODE));
+    new_top->item = item;
+    new_top->next = pilha->top;
+    pilha->top = new_top;
+    return;
+}
+
+bool is_empty(STACK *pilha){
+    if(pilha->top == NULL){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void pop(STACK *pilha){
+    if(is_empty(pilha)){
+        printf("EMPTY STACK\n");
+    }
+    else{
+        ELEMENT *retirar;
+        retirar = pilha->top;
+        print_lista(retirar->lista)
+        pilha->top = pilha->top->next;
+        free(retirar);
+    }
+}
